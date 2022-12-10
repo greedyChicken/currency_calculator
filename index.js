a023eb5e6960adfcd14227edf5d26619765a907b
Jan Hickiewicz <janhi@tutanota.com>
2022-12-11 00:25:17 +0100

    Refactoring

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-let amountElement = document.querySelector(".amount");
-let formElement = document.querySelector(".form");
-let fromCurrencySelect = document.querySelector("#fromCurrency");
-let toCurrencySelect = document.querySelector("#toCurrency");
-let resultElement = document.querySelector(".result");
+{
+    const calculateRate = () => {
+        let fromCurrencySelect = document.querySelector(".js-fromCurrency");
+        let toCurrencySelect = document.querySelector(".js-toCurrency");
 
-formElement.addEventListener("input", (event) => {
-    event.preventDefault();
+        let rate = 1;
+        let usdToPln = 4.44;
+        let usdToEur = 0.95;
+        let eurToPln = 4.67;
+        let eurToUsd = 1.05;
+        let plnToUsd = 0.23;
+        let plnToEur = 0.21;
 
-    let rate = 1;
-    let usdToPln = 4.44;
-    let usdToEur = 0.95;
-    let eurToPln = 4.67;
-    let eurToUsd = 1.05;
-    let plnToUsd = 0.23;
-    let plnToEur = 0.21;
-    
-    if (fromCurrencySelect.value === 'usd' && toCurrencySelect.value === 'eur') {
-        rate = usdToEur;
-    } else if (fromCurrencySelect.value === 'usd' && toCurrencySelect.value === 'pln') {
-        rate = usdToPln;
-    } else if (fromCurrencySelect.value === 'eur' && toCurrencySelect.value === 'usd') {
-        rate = eurToUsd;
-    } else if (fromCurrencySelect.value === 'eur' && toCurrencySelect.value === 'pln') {
-        rate = eurToPln;
-    } else if (fromCurrencySelect.value === 'pln' && toCurrencySelect.value === 'usd') {
-        rate = plnToUsd;
-    } else if (fromCurrencySelect.value === 'pln' && toCurrencySelect.value === 'eur') {
-        rate = plnToEur;
+        if (fromCurrencySelect.value === toCurrencySelect.value) {
+            return rate;
+        } else {
+            switch (fromCurrencySelect.value) {
+                case "usd": 
+                    return toCurrencySelect.value === "eur" ? usdToEur : usdToPln; 
+                case "eur":
+                    return toCurrencySelect.value === "usd" ? eurToUsd : eurToPln;
+                case "pln":
+                    return toCurrencySelect.value === "usd" ? plnToUsd : plnToEur;
+            }
+        }
     }
 
-    if (amountElement.value >= 0) {
-        resultElement.innerText = (amountElement.value * rate).toFixed(2);
+    const calculateResult = (rate) => {
+        let amountElement = document.querySelector(".amount");
+        let resultElement = document.querySelector(".result");
+
+        if (amountElement.value >= 0) {
+            resultElement.innerText = (amountElement.value * rate).toFixed(2);
+        }
     }
-})
+
+    const init = () => {
+        let formElement = document.querySelector(".form");
+
+        formElement.addEventListener("input", () => calculateResult(calculateRate()))
+    }
+
+    init();
+}
